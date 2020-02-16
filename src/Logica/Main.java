b96321ff5ee925ae9e06d604dd2e32908a4507cd
segamboa <47196444+segamboa@users.noreply.github.com>
2020-02-16 17:49:06 -0500

    Ella quiere beber ella quiere bailar, su novio la dejo y ella quiere olvidaaaar

--- src/Logica/Main.java
+++ src/Logica/Main.java
@@ -1,9 +1,40 @@
 package Logica;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		File file = new File("./data/parametros.txt"); 
+		int clientes;
+		int mensajes;
+		int servidores;
+		int tam_bufer;
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+
+			clientes=Integer.parseInt(br.readLine().split("=")[1]);
+			mensajes=Integer.parseInt(br.readLine().split("=")[1]);
+			servidores=Integer.parseInt(br.readLine().split("=")[1]);
+			tam_bufer=Integer.parseInt(br.readLine().split("=")[1]);
+			
+			br.close();
+			
+			System.out.println(clientes);
+			System.out.println(mensajes);
+			System.out.println(servidores);
+			System.out.println(tam_bufer);
+			
+		} catch (NumberFormatException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 
 	}
 
