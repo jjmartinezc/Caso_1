df3a366f54330fa1f892f91406c848eee03c3616
jjmartinezc <jj.martinezc@uniandes.edu.co>
2020-02-16 17:26:51 -0500

    Clase mensaje editada

--- src/Logica/Mensaje.java
+++ src/Logica/Mensaje.java
@@ -2,4 +2,43 @@ package Logica;
 
 public class Mensaje {
 
+	/**
+	 * Mensaje del mensaje (debe ser respondido)
+	 */
+	private int msg; 
+	
+	/**
+	 * Cliente que genera el mensaje
+	 */
+	private Cliente client;
+	
+	public Mensaje(Cliente c){
+		msg = 0;
+		client = c;
+	}
+	
+	/**
+	 * Responde el mensaje (agrega en 1 el valor del msg)
+	 */
+	public void responderMsg(){
+		msg++;
+	}
+	
+	/**
+	 * Retorna el cliente dueño del mensaje
+	 * @return client, cliente dueño del mensaje
+	 */
+	public Cliente darCliente(){
+		return client;
+	}
+	
+	/**
+	 * Retorna el valor del msg
+	 * @return msg, valor del mensaje (1 si ya fue respondido, 0 de lo contrario)
+	 */
+	public int darMsg(){
+		return msg;
+	}
+	
+	
 }
