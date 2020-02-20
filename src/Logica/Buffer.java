05d201edd03815164f0b0dc9131d224cc6904225
segamboa <47196444+segamboa@users.noreply.github.com>
2020-02-20 17:13:23 -0500

    Estas no son horas de mamar a menos que me lo quieras mamar

--- src/Logica/Buffer.java
+++ src/Logica/Buffer.java
@@ -1,10 +1,40 @@
 package Logica;
 
+import java.util.ArrayList;
+
 public class Buffer {
 
 	private int tamBuffer;
+	private ArrayList<Mensaje> mensajes;
+	private ArrayList<Mensaje> respuestas;
 	
 	public Buffer(int tamBuffer){
 		this.tamBuffer=tamBuffer;
+		mensajes = new ArrayList<Mensaje>();
+		respuestas = new ArrayList<Mensaje>();
+	}
+	
+	public void recibirMensaje(Mensaje msg){
+		while(mensajes.size()< tamBuffer){
+			synchronized(this){
+				mensajes.add(msg);
+				System.out.println("Se agregó el mensaje del cliente: " + msg.darCliente() + " al buffer");
+			}
+			synchronized(msg){
+				System.out.println("Se agregó al buffer la respuesta del servidor al mensaje del cliente: " + msg.darCliente());
+			}
+		}
+		synchronized(msg){
+		while(mensajes.size()==tamBuffer){
+				System.out.println("El buffer no tiene capacidad para recibir más solicitudes, por favor espere.");
+				try {
+					msg.wait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
 	}
 }
